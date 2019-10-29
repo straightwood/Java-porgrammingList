a5128d72a88f3db93ed9b6de0c4809bbb3aa54d3
straightwood <48054490+straightwood@users.noreply.github.com>
2019-10-29 18:19:59 +0800

    10.29-v2

--- chapter9_practice/src/chapter9_practice/TestLocation.java
+++ chapter9_practice/src/chapter9_practice/TestLocation.java
@@ -17,10 +17,36 @@ public class TestLocation {
 			}
 		}
 		input.close();
-		
-		Location local = new Location();
-		System.out.print("The location of the largest element is "+local.getMaxValue()+" at ("+local.getRow()+","+local.getColumn()+")");
 
+		//Location local = locateLargest(array);
+		System.out.print("The location of the largest element is "+locateLargest(array).maxValue+
+				" at ("+locateLargest(array).row+","+locateLargest(array).column+")");
 	}
-
+	public static Location locateLargest(double[][] a) {
+		int row = 0;
+		int column = 0;
+		double maxValue = a[0][0];
+		for(int i=0;i<a.length;i++) {
+			for(int j=0;j<a[0].length;j++) {
+				if(a[i][j]>maxValue) {
+					row = i;
+					column = j;
+					maxValue = a[i][j];
+				}
+			}
+		}
+		Location location = new Location();
+		location.row = row;
+		location.column = column;
+		location.maxValue = maxValue;
+		
+		return location;
+	
+	}
+}
+class Location {
+	
+	public int row = 0;
+	public int column = 0;
+	public double maxValue;
 }
